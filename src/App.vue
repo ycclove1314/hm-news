94e05b971b60a9a966f07e754515544efdfb49a7
unknown <1014372227@qq.com>
2020-06-30 18:05:08 +0800

    添加了轮播图等功能

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,79 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+       <div>
+       <van-button type="primary">主要按钮</van-button>
+       <van-field v-model="text" label="文本" />
+<!-- 输入手机号，调起手机号键盘 -->
+<van-field v-model="tel" type="tel" label="手机号" />
+<!-- 允许输入正整数，调起纯数字键盘 -->
+<van-field v-model="digit" type="digit" label="整数" />
+<!-- 允许输入数字，调起带符号的纯数字键盘 -->
+<van-field v-model="number" type="number" label="数字" />
+<!-- 输入密码 -->
+<van-field v-model="password" type="password" label="密码" />
+   </div>
+   <div class="box">哈哈哈</div>
+
+   <van-pagination
+  v-model="currentPage"
+  :total-items="125"
+  :show-page-size="3"
+  force-ellipses
+/>
+<hr>
+<van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+  <van-swipe-item>
+    <img src="../public/image/微信图片_20200316152026.jpg" alt="">
+  </van-swipe-item>
+  <van-swipe-item>
+     <img src="../public/image/微信图片_202003161520261.jpg" alt="">
+  </van-swipe-item>
+  <van-swipe-item>
+     <img src="../public/image/微信图片_202003161520262.jpg" alt="">
+  </van-swipe-item>
+  <van-swipe-item>
+     <img src="../public/image/微信图片_202003161520263.jpg" alt="">
+  </van-swipe-item>
+   <van-swipe-item>
+     <img src="../public/image/微信图片_202003161520264.jpg" alt="">
+  </van-swipe-item>
+</van-swipe>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      tel: '',
+      text: '',
+      digit: '',
+      number: '',
+      password: '',
+      currentPage: 1
     }
   }
 }
+</script>
+
+<style>
+  .box{
+      width: 150px;
+      height: 150px;
+      background-color: hotpink;
+      font-size: 50px;
+  }
+   .my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 100px;
+    text-align: center;
+    background-color: #39a9ed;
+    width: 150px;
+    height: 240px;
+  }
+   img{
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-
+/* 导入amfe-flexible 适配rem插件 */
+import 'amfe-flexible'
+/* 自动按需导入导入Button按钮插件 */
+import { Button, Field, Pagination, Swipe, SwipeItem } from 'vant'
 Vue.config.productionTip = false
-
+/* 使用Button插件 */
+Vue.use(Button)
+/* 使用Field插件 */
+Vue.use(Field)
+Vue.use(Pagination)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
